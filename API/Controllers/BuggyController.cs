f0ca7fa7803d36523e10c2b34661e0fcc4dff2de
Miraj Mohajireen <miraj0072004@gmail.com>
2025-02-24 10:30:17 +1100

    End of section 6

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized(){
+            return Unauthorized();
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest(){
+            return BadRequest("Not a good request");
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFound(){
+            return NotFound();
+        }
+
+        [HttpGet("internalerror")]
+        public IActionResult GetInternalError(){
+            throw new Exception("this is a test exception");
+        }
+
+        [HttpPost("validationerror")]
+        public IActionResult GetValidationError(CreateProductDto product){
+            return Ok();
+        }
+    }
+}
